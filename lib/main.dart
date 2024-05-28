eaa1c8096a33e68f7595855ba7437a8b2cbbc08b
Sara-Waleed <sarawaleedmohamed2@gmail.com>
2024-05-28 13:06:10 +0300

    Adding Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro/view/screens/Home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+   MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+final ThemeData lightTheme=
+    ThemeData(
+      primarySwatch: Colors.lightBlue,
+      brightness: Brightness.light,
+      appBarTheme: AppBarTheme(
+        color: Colors.lightBlue,
+      )
+    );
+
+   final ThemeData DarkTheme=
+   ThemeData(
+       primarySwatch: Colors.blue,
+       brightness: Brightness.dark,
+       appBarTheme: AppBarTheme(
+         color: Colors.black,
+       )
+   );
+
+   bool isDark= false;
+  void toggleTheme(){
+    setState(() {
+      isDark=!isDark;
+    });
+  }
+
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Home(),
+      debugShowCheckedModeBanner: false,
+      theme:lightTheme ,
+      darkTheme: DarkTheme,
+      themeMode: isDark? ThemeMode.dark : ThemeMode.light,
+      home: Home(
+        isDark: isDark,
+        Toggle: toggleTheme,
+      ),
     );
   }
 }
--- lib/view/screens/Home.dart
+++ lib/view/screens/Home.dart
@@ -2,12 +2,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-
+   Home({Key? key, required this.isDark, required this.Toggle}) : super(key: key);
+   final VoidCallback Toggle;
+   final bool isDark;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text('Theme'),
+        actions: [
+          IconButton(
+            icon: Icon(isDark ? Icons.wb_sunny : Icons.nights_stay),
+            onPressed: Toggle,
+          ),
+        ],
+      ),
     );
   }
 }
