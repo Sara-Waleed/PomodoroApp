996b666a155207739a5ac63fb2c346792527c27a
Sara-Waleed <sarawaleedmohamed2@gmail.com>
2024-06-01 14:39:20 +0300

    1st

--- lib/view/Main_Widgets/HomeBody.dart
+++ lib/view/Main_Widgets/HomeBody.dart
@@ -17,64 +17,71 @@ class _HomeBodyState extends State<HomeBody> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Pomodoro Setup")),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 38),
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Text("Work Duration", style: TextStyle(fontSize: 25)),
-              SizedBox(height: 15),
-              TextField(
-                controller: workDurationController,
-                decoration: InputDecoration(hintText: "(In Minutes)"),
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(height: 30),
-              Text("Break Duration", style: TextStyle(fontSize: 25)),
-              SizedBox(height: 15),
-              TextField(
-                controller: breakDurationController,
-                decoration: InputDecoration(hintText: "(In Minutes)"),
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(height: 30),
-              Text("Sessions", style: TextStyle(fontSize: 25)),
-              SizedBox(height: 15),
-              TextField(
-                controller: sessionsController,
-                decoration: InputDecoration(hintText: "(Number of work sessions)"),
-                keyboardType: TextInputType.number,
+              Column(
+                children: [
+                  Text("Work Duration", style: TextStyle(fontSize: 25)),
+                  SizedBox(height: 15),
+                  TextField(
+                    controller: workDurationController,
+                    decoration: InputDecoration(hintText: "(In Minutes)"),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(height: 30),
+                  Text("Break Duration", style: TextStyle(fontSize: 25)),
+                  SizedBox(height: 15),
+                  TextField(
+                    controller: breakDurationController,
+                    decoration: InputDecoration(hintText: "(In Minutes)"),
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(height: 30),
+                  Text("Sessions", style: TextStyle(fontSize: 25)),
+                  SizedBox(height: 15),
+                  TextField(
+                    controller: sessionsController,
+                    decoration: InputDecoration(hintText: "(Number of work sessions)"),
+                    keyboardType: TextInputType.number,
+                  ),
+                ],
               ),
               SizedBox(height: 50),
-              GestureDetector(
-                onTap: () {
-                  int workDuration = int.parse(workDurationController.text);
-                  int breakDuration = int.parse(breakDurationController.text);
-                  int sessions = int.parse(sessionsController.text);
+              Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      int workDuration = int.parse(workDurationController.text);
+                      int breakDuration = int.parse(breakDurationController.text);
+                      int sessions = int.parse(sessionsController.text);
 
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SessionScreen(
-                        workDuration: workDuration,
-                        breakDuration: breakDuration,
-                        sessions: sessions,
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SessionScreen(
+                            workDuration: workDuration,
+                            breakDuration: breakDuration,
+                            sessions: sessions,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 180,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                        child: Text("Start", style: TextStyle(fontSize: 25, color: Colors.white)),
                       ),
                     ),
-                  );
-                },
-                child: Container(
-                  height: 50,
-                  width: 180,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(20),
                   ),
-                  child: Center(
-                    child: Text("Start", style: TextStyle(fontSize: 25, color: Colors.white)),
-                  ),
-                ),
+                ],
               ),
             ],
           ),
