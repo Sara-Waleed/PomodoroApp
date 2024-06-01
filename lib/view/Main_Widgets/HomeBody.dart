bca0551ac3c7a78b68a402c0bd3112d3d3b18ec5
Sara-Waleed <sarawaleedmohamed2@gmail.com>
2024-06-01 14:59:46 +0300

    1st

--- lib/view/Main_Widgets/HomeBody.dart
+++ lib/view/Main_Widgets/HomeBody.dart
@@ -13,7 +13,7 @@ class _HomeBodyState extends State<HomeBody> {
   final TextEditingController workDurationController = TextEditingController();
   final TextEditingController breakDurationController = TextEditingController();
   final TextEditingController sessionsController = TextEditingController();
-
+  final _formKey=GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,66 +22,102 @@ class _HomeBodyState extends State<HomeBody> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Column(
-                children: [
-                  Text("Work Duration", style: TextStyle(fontSize: 25)),
-                  SizedBox(height: 15),
-                  TextField(
-                    controller: workDurationController,
-                    decoration: InputDecoration(hintText: "(In Minutes)"),
-                    keyboardType: TextInputType.number,
-                  ),
-                  SizedBox(height: 30),
-                  Text("Break Duration", style: TextStyle(fontSize: 25)),
-                  SizedBox(height: 15),
-                  TextField(
-                    controller: breakDurationController,
-                    decoration: InputDecoration(hintText: "(In Minutes)"),
-                    keyboardType: TextInputType.number,
-                  ),
-                  SizedBox(height: 30),
-                  Text("Sessions", style: TextStyle(fontSize: 25)),
-                  SizedBox(height: 15),
-                  TextField(
-                    controller: sessionsController,
-                    decoration: InputDecoration(hintText: "(Number of work sessions)"),
-                    keyboardType: TextInputType.number,
-                  ),
-                ],
+              Form(
+                key: _formKey,
+
+                child: Column(
+                  children: [
+                    Text("Work Duration", style: TextStyle(fontSize: 25)),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      validator: (value) {
+                        if(value!.isEmpty ){
+                          return "please enter work duration ";
+                        }
+                        return null;
+                      },
+                      controller: workDurationController,
+                      decoration: InputDecoration(
+
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          hintText: "(In Minutes)"),
+                      keyboardType: TextInputType.number,
+                    ),
+                    SizedBox(height: 30),
+                    Text("Break Duration", style: TextStyle(fontSize: 25)),
+                    SizedBox(height: 15),
+                    TextFormField(validator: (value) {
+                      if(value!.isEmpty  ){
+                        return "please enter break duration ";
+                      }
+                      return null;
+                    },
+                      controller: breakDurationController,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          hintText: "(In Minutes)"),
+                      keyboardType: TextInputType.number,
+                    ),
+                    SizedBox(height: 30),
+                    Text("Sessions", style: TextStyle(fontSize: 25)),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      validator: (value) {
+                        if(value!.isEmpty ){
+                          return "please enter session number ";
+                        }
+                        return null;
+                      },
+                      controller: sessionsController,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          hintText: "(Number of work sessions)"),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ],
+                ),
               ),
               SizedBox(height: 50),
-              Column(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      int workDuration = int.parse(workDurationController.text);
-                      int breakDuration = int.parse(breakDurationController.text);
-                      int sessions = int.parse(sessionsController.text);
+              GestureDetector(
+                onTap: () {
+                  if (_formKey.currentState!.validate()) {
+                    // If the form is valid, display a snackbar
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Processing Data')),
+                    );
+                  }
+                  int workDuration = int.parse(workDurationController.text);
+                  int breakDuration = int.parse(breakDurationController.text);
+                  int sessions = int.parse(sessionsController.text);
 
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SessionScreen(
-                            workDuration: workDuration,
-                            breakDuration: breakDuration,
-                            sessions: sessions,
-                          ),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      height: 50,
-                      width: 180,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Center(
-                        child: Text("Start", style: TextStyle(fontSize: 25, color: Colors.white)),
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SessionScreen(
+                        workDuration: workDuration,
+                        breakDuration: breakDuration,
+                        sessions: sessions,
                       ),
                     ),
+                  );
+                },
+                child: Container(
+                  height: 50,
+                  width: 180,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                ],
+                  child: Center(
+                    child: Text("Start", style: TextStyle(fontSize: 25, color: Colors.white)),
+                  ),
+                ),
               ),
             ],
           ),
@@ -90,3 +126,4 @@ class _HomeBodyState extends State<HomeBody> {
     );
   }
 }
+
